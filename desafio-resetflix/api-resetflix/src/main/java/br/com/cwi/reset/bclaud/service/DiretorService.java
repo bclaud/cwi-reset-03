443aedc4dd87a8d87b38d4e155851f37c2fd5ac0
bclaud <bruarrais@gmail.com>
2021-10-12 19:38:33 -0300

    adicionado exceptions de diretor

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/bclaud/service/DiretorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/bclaud/service/DiretorService.java
@@ -1,8 +1,10 @@
 package br.com.cwi.reset.bclaud.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import br.com.cwi.reset.bclaud.exceptions.DiretorExceptions;
 import br.com.cwi.reset.bclaud.models.Diretor;
 import br.com.cwi.reset.bclaud.repositories.Repository;
 
@@ -13,28 +15,69 @@ public class DiretorService {
         this.diretorRepository = diretorRepository;
     }
 
-    public void cadastrarDiretor(Diretor diretor){
-        //TODO custom exceptions
+    public void cadastrarDiretor(Diretor diretor) throws DiretorExceptions{
+        //TODO falta exception do campo
+        if(isDuplicated(diretor)){
+            throw new DiretorExceptions("Já existe um diretor cadastrado para o nome " + diretor.getNome() +".");
+        }
+        if(!isValidBirthdate(diretor)){
+            throw new DiretorExceptions("Não é possível cadastrar diretores não nascidos.");
+        }
+        if(!isValidActivityStartdate(diretor)){
+            throw new DiretorExceptions("Ano de início de atividade inválido para o diretor cadastrado.");
+        }
+        if(!isValidName(diretor)){
+            throw new DiretorExceptions("Deve ser informado no mínimo nome e sobrenome para o diretor.");
+        }
+
         diretor.setId(idGenerator());
         diretorRepository.salvarDiretor(diretor);
     }
 
-    public List<Diretor> listarDiretores(String filtroNome){
-        //TODO custom exceptions
-        return diretorRepository.listarDiretores().stream()
+    public List<Diretor> listarDiretores(String filtroNome) throws DiretorExceptions{
+        List<Diretor> listaDiretoresFiltrado = diretorRepository.listarDiretores().stream()
         .filter(diretor -> diretor.getNome().toUpperCase().contains(filtroNome.toUpperCase()))
         .collect(Collectors.toList());
+
+        if(listaDiretoresFiltrado.isEmpty()){
+            throw new DiretorExceptions("Diretor não encontrato com o filtro "+ filtroNome +", favor informar outro filtro.");
+        }
+
+        return listaDiretoresFiltrado;
     }
 
-    public List<Diretor> listarDiretores(){
-        //TODO custom exceptions
-        return diretorRepository.listarDiretores().stream()
+    public List<Diretor> listarDiretores() throws DiretorExceptions{
+        List<Diretor> listaDiretor = diretorRepository.listarDiretores().stream()
         .collect(Collectors.toList());
+
+        if(listaDiretor.isEmpty()){
+            throw new DiretorExceptions("Nenhum diretor cadastrado, favor cadastar diretores.");
+        }
+
+        return listaDiretor;
     }
 
-    public Diretor consultarDiretor(Long id){
-        //TODO custom exceptions
-        return diretorRepository.consultarDiretor(id).get();
+    public Diretor consultarDiretor(Long id) throws DiretorExceptions{
+        //TODO falta exception campo obrigatorio
+        return diretorRepository.consultarDiretor(id)
+        .orElseThrow(() -> new DiretorExceptions("Nenhum diretor encontrado com o parâmetro id="+id+", favor verifique os parâmetros informados."));
+    }
+
+    public boolean isDuplicated(Diretor diretor){
+        return diretorRepository.listarDiretores().stream()
+        .anyMatch(d -> d.getNome().equalsIgnoreCase(diretor.getNome()));
+    }
+
+    public boolean isValidBirthdate(Diretor diretor){
+        return diretor.getDataNascimento().isBefore(LocalDate.now());
+    }
+
+    public boolean isValidActivityStartdate(Diretor diretor){
+        return diretor.getAnoInicioAtividade() > diretor.getDataNascimento().getYear();
+    }
+
+    public boolean isValidName(Diretor diretor){
+        return diretor.getNome().contains(" ");
     }
 
     public Long idGenerator(){
