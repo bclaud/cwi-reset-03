75a067bd645a15901615baa235753ef6aac20755
bclaud <bruarrais@gmail.com>
2021-10-18 20:30:16 -0300

    adicionado http bad_request nas exceptions

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/bclaud/apiresetflix/exceptions/AtorExceptions.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/bclaud/apiresetflix/exceptions/AtorExceptions.java
@@ -1,5 +1,9 @@
 package br.com.cwi.reset.bclaud.apiresetflix.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class AtorExceptions extends RuntimeException{
     
     public AtorExceptions(String msg){
